d09c77bda064e3aef9b50e09e0838f1cde5440dd
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-18 17:19:15 -0500

    set form type submit, start regex auth for user

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const viewPassword = document.getElementById("bi-eye-slash"); // BootStrap Icon 
 const passwordInput = document.getElementById("passwordInput"); // Input Password.
 const usernameInput = document.getElementById("usernameInput"); // Username input.
 
+
+// View Password option with Bootstrap Icons
 viewPassword.addEventListener("click", function() {
     const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
     passwordInput.setAttribute("type", type);
     this.classList.toggle("bi-eye");
- })
+ });
+ 
+
+ // Check Authentication for the inputs provided.
+ // Uses Regex...
+ function checkVerify() {
+    
+ }
 
 
 
