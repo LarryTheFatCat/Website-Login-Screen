a4e6ba2cf5c6565ac2c3039912957f231eaee52e
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-18 23:33:44 -0500

    tested something new?!?! lol that's cool

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const viewPassword = document.getElementById("bi-eye-slash"); // BootStrap Icon For Eye Slash password.
 const passwordInput = document.getElementById("passwordInput"); // Input Password.
 const usernameInput = document.getElementById("usernameInput"); // Username input.
+const button = document.getElementById("log"); // Login Button.
+const errorMessageUsername = document.getElementById("errormessage"); // Error Message username.
+const errorMessagePassword = document.getElementById("errormessage-password"); // Error Message password.
 
 // View Password option with Bootstrap Icons
 viewPassword.addEventListener("click", function() {
@@ -8,5 +11,15 @@ viewPassword.addEventListener("click", function() {
     passwordInput.setAttribute("type", type);
     this.classList.toggle("bi-eye"); 
 });
+
+button.addEventListener("click", function() {
+    if(usernameInput.value == "" || passwordInput.value == "") {
+        errorMessageUsername.style.display = "block";
+        errorMessagePassword.style.display = "block";
+    }
+});
+
+
+
 
 
