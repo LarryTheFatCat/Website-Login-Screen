2396895272f2592bd0227e01cd2f03ad7578d66d
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-18 23:49:55 -0500

    functional auth... positions are messed up.

--- script.js
+++ script.js
@@ -12,6 +12,31 @@ viewPassword.addEventListener("click", function() {
     this.classList.toggle("bi-eye"); 
 });
 
+// Testing project. fucking gotta fix the user input stuff...
+/*
+function checkVerify() {
+    let errorMessageUsername = document.getElementById("errormessage"); // Error Message username.
+    let errorMessagePassword = document.getElementById("errormessage-password"); // Error Message password.
+    let validation;
+    let regexUsername = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    let regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+    
+    validation = regexUsername.test(usernameInput.value);
+    validation = regexPassword.test(passwordInput.value);
+    console.log(validation);
+
+    if(validation == false) {
+        button.addEventListener("click", function() {
+            if(usernameInput.value == "" || passwordInput.value == "") {
+                errorMessageUsername.style.display = "block";
+                errorMessagePassword.style.display = "block";
+            }
+        });
+    } else {
+        console.log("You are logged in");
+    }
+}
+*/
 
 // Use this for regex auth purpose
 /**
