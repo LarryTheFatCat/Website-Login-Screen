3727de10ddde108a78a52feedb865d85e0b11904
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-24 19:51:21 -0500

    move around javascript code

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const button = document.getElementById("log"); // Login Button.
 const errorMessageUsername = document.getElementById("errormessage"); // Error Message username.
 const errorMessagePassword = document.getElementById("errormessage-password"); // Error Message password.
 
+// Main onclick function for verification
+function checkVerify() { 
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // Determines if user correctly inputs email
+    const validateEmail = emailRegex.test(usernameInput.value);
+
+    if(passwordInput.value.length < 8 && validateEmail === false) { // Checks for incorrect input on click
+        errorMessageUsername.style.display = "block";
+        errorMessagePassword.style.display = "block";
+    } else if(passwordInput.value.length > 8 && validateEmail === true  ){ // Checks for correct output after click
+        errorMessageUsername.style.display = "none";
+        errorMessagePassword.style.display = "none";
+    }
+}
+
 // View Password option with Bootstrap Icons
 viewPassword.addEventListener("click", function() {
     const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
     passwordInput.setAttribute("type", type);
     this.classList.toggle("bi-eye"); 
 });
-
-
-function checkVerify() {
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const validateEmail = emailRegex.test(usernameInput.value);
-
-    if(passwordInput.value.length < 8 || validateEmail === false) {
-        errorMessageUsername.style.display = "block";
-        errorMessagePassword.style.display = "block";
-    } else if(passwordInput.value.length > 8 || validateEmail === true  ){
-        errorMessageUsername.style.display = "none";
-        errorMessagePassword.style.display = "none";
-    }
-}
